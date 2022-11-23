d52ce6e1cf68e4137274b809da07ba0af74c9868
evance-14 <kiropevans097@gmail.com>
2022-11-23 10:14:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let msg;
+  if(feet > 2500){
+    msg = 'No can do.';
+  }else if(feet > 2000){
+    msg = 'I will gladly take your thirty bucks.';
+  }else if(feet <= 400){
+    msg = 'This one is on me!';
+  }else{
+  }
+  return msg;
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC") ? "Ok, sounds good." : 'No go.';
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let response;
+  switch(tip){
+    case 'generous':
+      response = "Thank you so much."
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response;
+}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
